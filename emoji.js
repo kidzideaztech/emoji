fec56330b90c01b330eb733d358616b64f98293c
Nathaniel Dsouza <nate@kidzideaz.xyz>
2018-01-31 16:29:36 -0500

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,7 +1,10 @@
+// This code is designed to work on MeteorJS apps with the HTTP package.
+// You may need to modify the function at line 4 to whatever function you want to use to make the HTTP call
+// This call will get the latest emoji map, allowing you to integerate new emoji without reloading your app
 Emoji = function (str) {
     var future = new Future();
-    HTTP.call("GET", "https://raw.githubusercontent.com/kidzideaztech/emoji/master/emoji.txt", function (err, currentVersion) {
-        map = JSON.parse(currentVersion.content);
+    HTTP.call("GET", "https://raw.githubusercontent.com/kidzideaztech/emoji/master/emoji.txt", function (err, response) {
+        map = JSON.parse(response.content);
         
         data = str.split(" ").map( function(num ){
                 return map.hasOwnProperty(num) ? map[num] : num;
