40547702ae30d8caa8501686d3905eba8df5248a
Nathaniel Dsouza <nate@kidzideaz.xyz>
2018-02-09 11:27:21 -0500

    Now using try/catch instead of Future.wait.
    No longer requires future dependency.

--- emoji.js
+++ emoji.js
@@ -2,14 +2,22 @@
 // You may need to modify the function at line 4 to whatever function you want to use to make the HTTP call
 // This call will get the latest emoji map, allowing you to integerate new emoji without reloading your app
 Emoji = function (str) {
-    var future = new Future();
-    HTTP.call("GET", "https://raw.githubusercontent.com/kidzideaztech/emoji/master/emoji.txt", function (err, response) {
-        map = JSON.parse(response.content);
+    try
+    {
+        const result = HTTP.call('GET', 'https://raw.githubusercontent.com/kidzideaztech/emoji/master/emoji.txt');
+
+        map = JSON.parse(result.content);
         
         data = str.split(" ").map( function(num ){
-                return map.hasOwnProperty(num) ? map[num] : num;
-            }).join(" ");
-        future["return"](data);
-    });
-    return future.wait();    
+            return map.hasOwnProperty(num) ? map[num] : num;
+        }).join(" ");
+
+        return data;
+
+    }
+    catch (e)
+    {
+        console.error("Could not fetch emoji map");
+        return str;
+    }    
 }
